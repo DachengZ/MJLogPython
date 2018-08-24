21654fcc3b9c8e2dfe38be351702b5b640675bbe
DachengZ <shs.10978@gmail.com>
2018-08-23 20:52:33 -0700

    Added sample replays

--- replay.py
+++ replay.py
@@ -1,19 +1,9 @@
-url = '/Users/dacheng/Documents/TenhouMJLog/log/201706/2017062919gm-0089-0000-0c6b375e&tw=1&ts=0.mjlog'
 import xml.etree.ElementTree as ET
-readxml = ET.parse(url)
-root = readxml.getroot()
-root.tag, root.attrib
-for elem in readxml.iter():
-    print(elem.tag, elem.attrib)
-
-MT = readxml.find('SHUFFLE').get('seed')
-elem.find('seed')
-
 import os
-os.chdir('/Users/Dacheng/Documents/MJLogPython/')
+os.chdir(os.path.expanduser('~/Documents/MJLogPython/'))
 from printHaipai import PaifuParseTool
-a = PaifuParseTool('2010020113gm-0089-0000-b85fd1cc&tw=3&ts=0.mjlog')
+a = PaifuParseTool('2017073123gm-0029-0000-be75e007&tw=0&ts=7.mjlog')
+#a = PaifuParseTool('2010020113gm-0089-0000-b85fd1cc&tw=3&ts=0.mjlog')
+#a = PaifuParseTool('2009071817gm-00c1-0000-3a507b6f&tw=2&ts=2.mjlog')
+#a = PaifuParseTool('2009070522gm-00c1-0000-a4fd12c0&tw=1&ts=0.mjlog')
 a.printHaiPai()
-
-'mt19937ar' in sys.modules
-'ET' in sys.modules
